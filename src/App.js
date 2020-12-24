ef9577646e2a6bd57a28c3e4e2d46e5402f6411a
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-24 08:54:56 -0500

    modified Nav for conditional rendering of About and Portfolio with space holders for Contact and Resume

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React, {useState} from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 
 function App() {
+  const [sections] = useState(['About Me', 'Portfolio', 'Contact', 'Resume'])
+  const[currentSection, setCurrentSection] = useState(sections[0]);
   return (
     <div>
-      <Nav />
+      <Nav sections={sections} setCurrentSection={setCurrentSection} currentSection={currentSection} />
       <main>
-        <About />
-        <Portfolio />
+        {(currentSection === 'About Me') ? <About />
+        : (currentSection === 'Portfolio') ? <Portfolio />
+        : (currentSection === 'Contact') ? <About />
+        : (currentSection === 'Resume') ? <About />
+        : <About />}
       </main>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-function Nav() {
+function Nav(props) {
+    const {
+        sections = [],
+        setCurrentSection,
+        currentSection
+    } = props;
+    useEffect(() => {
+        document.title = 'Kathleen Pehl - ' + currentSection
+    }, [currentSection]);
     return (
         <header>
             <h2><a href = "/">Home</a></h2>
             <nav>
                 <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#about">About me</a>
+                {sections.map((section) => (
+                    <li className={`mx-2 ${
+                        currentSection === section && 'navActive'
+                        }`} key={section}>
+                    <span
+                        onClick={() => {
+                        setCurrentSection(section)
+                        }}
+                    >
+                        {section}
+                    </span>
                     </li>
-                    <li className="mx-2">
-                        Projects
-                    </li>
-                    <li className="mx-2">
-                        Contact me
-                    </li>
-                    <li className="mx-2">
-                        Resume
-                    </li>
+                ))}
                 </ul>
             </nav>
         </header>
