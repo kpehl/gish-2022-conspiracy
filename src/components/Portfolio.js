e33a5cd87f6ed07c3ac781821eb5698ad8e27ce2
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-25 16:20:49 -0500

    added json file for project information, modified Portfolio and Project components to display projects using data from json file; verified that portfolio page renders with two test projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Project from './Project';
+import projects from '../assets/projects.json';
 
 function Portfolio () {
+    console.log(projects);
+    const [projectList, setProjectList] = useState(projects);
+    console.log(projectList);
     return (
         <div className="portfolio">
-            <h2>My Portfolio</h2>
-            <div className="project">
-                <Project />
+            <h2 id="portfolio">My Portfolio</h2>
+            <div className="projects flex-row">
+                {projectList.map((project) => (
+                    <Project project={project} />
+                ))}
             </div>
         </div>
     );
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function Project () {
+function Project (props) {
+    console.log(props)
     return (
-        <h3>Project Title</h3>
+        <div className="project" id={props.project.id} key={props.project.id}>
+            <h3 className="project-title">{props.project.title}</h3>
+            <img src={props.project.image} alt={props.project.title} />
+            <a href={props.project.gitLink} target="_blank">GitHub Repository</a>
+            <a href={props.project.deployedLink}>Deployed Application</a>
+        </div>
     );
 }
 
