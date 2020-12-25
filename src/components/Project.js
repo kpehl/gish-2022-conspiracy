dde5c068795cc64a928e48cb807f09126511fa61
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-25 16:25:00 -0500

    added rel noreferrer to links to help prevent malicious redirection

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,8 +6,8 @@ function Project (props) {
         <div className="project" id={props.project.id} key={props.project.id}>
             <h3 className="project-title">{props.project.title}</h3>
             <img src={props.project.image} alt={props.project.title} />
-            <a href={props.project.gitLink} target="_blank">GitHub Repository</a>
-            <a href={props.project.deployedLink}>Deployed Application</a>
+            <a href={props.project.gitLink} target="_blank" rel="noreferrer">GitHub Repository</a>
+            <a href={props.project.deployedLink} target="_blank" rel="noreferrer">Deployed Application</a>
         </div>
     );
 }
