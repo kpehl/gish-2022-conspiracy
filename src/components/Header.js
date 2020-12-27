a0b35ec4f9e809c90d38cc256802513e8168cbc1
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-27 16:18:54 -0500

    added name to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
     return (
         <header className="header">
-            <h2><a href = "/">Home</a></h2>
+            <h2><a href = "/">Kathleen Pehl</a></h2>
             {props.children}
             <div className="hero"></div>
         </header>
