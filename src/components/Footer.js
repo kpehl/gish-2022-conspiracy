5a449b33c5098f606135aa8cdb846e77d7ab43ff
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-27 16:16:48 -0500

    added profile links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,11 @@ function Footer () {
     return (
         <footer>
             <div className="copyright">
-                <p>Copyright &copy; {(new Date().getFullYear())}</p>
+                <p>Copyright &copy; Kathleen Pehl, {(new Date().getFullYear())}</p>
+                <p><span className="icon"><i className="fab fa-linkedin"></i></span>{" "}<a href="https://www.linkedin.com/in/kathleen-pehl-77bb1710" target="_blank" rel="noreferrer">LinkedIn Profile</a>{" "}
+                    <span className="icon"><i className="fab fa-github"></i></span>{" "}<a href="https://github.com/kpehl" target="_blank" rel="noreferrer">GitHub Profile</a>{" "}
+                    <span className="icon"><i className="fab fa-stack-overflow"></i></span>{" "}<a href="https://stackoverflow.com/users/13924519/kpehl" target="_blank" rel="noreferrer">Stack Overflow Profile</a>
+                </p>
             </div>
         </footer>
     )
