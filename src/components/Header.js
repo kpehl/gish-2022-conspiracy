3b9b24d90c71ac4cbc8b104eef159128e1026ccf
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-26 20:17:50 -0500

    added styling and hero image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function Header(props) {
     return (
-        <header>
+        <header className="header">
             <h2><a href = "/">Home</a></h2>
             {props.children}
+            <div className="hero"></div>
         </header>
     );
 }
