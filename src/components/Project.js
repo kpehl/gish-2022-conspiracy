aa6b41ef0acc1b1294500048d0eb7356916f52b9
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-28 14:47:36 -0500

    installed react-bootstrap and converted Project component to use cards  for the individual projects; tweaked index.css for fonts in card; imported bootstrap CDN into index.html

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+// function Project (props) {
+//     console.log(props)
+//     return (
+//         <div className="project" id={props.project.id} key={props.project.id}>
+//             <h4 className="project-title">{props.project.title}</h4>
+//             <img src={props.project.image} alt={props.project.title} />{<br></br>}
+//             <p>{props.project.description}</p>
+//             <p>{props.project.tools}</p>
+//             <a href={props.project.gitLink} target="_blank" rel="noreferrer">GitHub Repository</a>{" "}
+//             <a href={props.project.deployedLink} target="_blank" rel="noreferrer">Deployed Application</a>
+//         </div>
+//     );
+// }
 
 function Project (props) {
-    console.log(props)
     return (
-        <div className="project" id={props.project.id} key={props.project.id}>
-            <h4 className="project-title">{props.project.title}</h4>
-            <img src={props.project.image} alt={props.project.title} />{<br></br>}
-            <p>{props.project.description}</p>
-            <p>{props.project.tools}</p>
-            <a href={props.project.gitLink} target="_blank" rel="noreferrer">GitHub Repository</a>{" "}
-            <a href={props.project.deployedLink} target="_blank" rel="noreferrer">Deployed Application</a>
-        </div>
+        <Card style={{ width: '45rem' }}>
+            <Card.Img variant="top" src={props.project.image} alt={props.project.title} />
+            <Card.Body>
+                <Card.Title>{props.project.title}</Card.Title>
+                <Card.Text>
+                    <p>{props.project.description}</p>
+                    <p>{props.project.tools}</p>                    
+                </Card.Text>
+            </Card.Body>
+            <Card.Body>
+                <Card.Link href={props.project.gitLink} target="_blank" rel="noreferrer">GitHub Repository</Card.Link>
+                <Card.Link href={props.project.deployedLink} target="_blank" rel="noreferrer">Deployed Application</Card.Link>
+            </Card.Body>
+        </Card>
     );
 }
 
