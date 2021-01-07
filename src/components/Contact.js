382445733617e59cc08eabed4e1d7eecfd7d7ad0
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-01-07 07:58:24 -0500

    updated Contact page with CSS animation for icons, background for the columns, and other stylistic changes; updated error messaging for email form; updated header home link to correctly load the react portfolio and updated resume link to match that change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,13 +10,13 @@ function Contact() {
         if (event.target.name === 'email') {
             const isValid = validateEmail(event.target.value);
             if(!isValid) {
-                setErrorMessage('Your email is invalid.');
+                setErrorMessage('Enter a valid email.');
             } else {
                 setErrorMessage('');
             }
         } else {
             if (!event.target.value.length) {
-                setErrorMessage(`${event.target.name} is required.`);
+                setErrorMessage(`A ${event.target.name} is required.`);
             } else {
                 setErrorMessage('');
             }
@@ -30,53 +30,101 @@ function Contact() {
         console.log(formState);
     }
     return (
-        <section>
+        <section id="contact" className="contact">
+            <div className="container">
             <h1>Contact me</h1>
-            <div className="contact-info">
-                <div className = "contact-column">
-                    <div className="contact-info-item">
-                        <h5>Location:</h5>
-                        <p>Fleming Island, FL 32003</p>
-                    </div>
-                    <div className="contact-info-item">
-                        <span className="icon"><i className="fas fa-mobile-alt"></i></span>{" "} <a href="tel:+14259536252">(425) 953-6252</a>
-                    </div>
-                    <div className="contact-info-item">
-                        <span className="icon"><i className="far fa-envelope"></i></span>{" "}<a href="mailto:kathleen.pehl@gmail.com">kathleen.pehl@gmail.com</a>
-                    </div>
-                    <div className="contact-info-item">
-                        <span className="icon"><i className="fab fa-linkedin"></i></span>{" "}<a href="https://www.linkedin.com/in/kathleen-pehl-77bb1710" target="_blank" rel="noreferrer">LinkedIn Profile</a>
-                    </div>
-                    <div className="contact-info-item">
-                        <span className="icon"><i className="fab fa-github"></i></span>{" "}<a href="https://github.com/kpehl"  target="_blank" rel="noreferrer">GitHub Profile</a>
+            <div className="contact-info row">
+                <div className = "contact-column col-lg-5 d-flex align-items-stretch">
+                    <div className = "info">
+                        <div className="contact-info-item">
+                            <span className="icon"><i className="fas fa-map-marker-alt"></i></span>
+                            <p>Location: Fleming Island, Florida 32003</p>
+                        </div>
+                        <div className="contact-info-item">
+                            <span className="icon"><i className="fas fa-mobile-alt"></i></span>{" "} <p><a href="tel:+14259536252">(425) 953-6252</a></p>
+                        </div>
+                        <div className="contact-info-item">
+                            <span className="icon"><i className="far fa-envelope"></i></span>{" "}<p><a href="mailto:kathleen.pehl@gmail.com">kathleen.pehl@gmail.com</a></p>
+                        </div>
+                        <div className="contact-info-item">
+                            <span className="icon"><i className="fab fa-linkedin"></i></span>{" "}<p><a href="https://www.linkedin.com/in/kathleen-pehl-77bb1710" target="_blank" rel="noreferrer">LinkedIn Profile</a></p>
+                        </div>
+                        <div className="contact-info-item">
+                            <span className="icon"><i className="fab fa-github"></i></span>{" "}<p><a href="https://github.com/kpehl"  target="_blank" rel="noreferrer">GitHub Profile</a></p>
+                        </div>
                     </div>
                 </div>
-                <div className="contact-column">
-                    <form id="contact-form" onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="name">Name:</label>
-                            <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="email">Email address:</label>
-                            <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="subject">Subject:</label>
-                            <input type="text" name="subject" defaultValue={subject} onBlur={handleChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="message">Message:</label>
-                            <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
-                        </div>
-                        {errorMessage && (
-                            <div>
-                                <p className="error-text">{errorMessage}</p>
+                <div className="contact-column col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                    <form className="php-email-form" onSubmit={handleSubmit}>
+                        <div className="row">
+                            <div className="form-group">
+                                <label htmlFor="name">Your Name</label>
+                                <input
+                                type="text"
+                                name="name"
+                                defaultValue={name}
+                                onBlur={handleChange}
+                                className="form-control"
+                                id="name"
+                                />
+                                <div className="validate"></div>
                             </div>
-                        )}
-                        <button type="submit">Submit</button>
+                            <div className="form-group">
+                                <label htmlFor="name">Your Email</label>
+                                <input
+                                type="email"
+                                name="email"
+                                defaultValue={email}
+                                onBlur={handleChange}
+                                className="form-control"
+                                id="email"
+                                />
+                                <div className="validate"></div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="form-group">
+                            <label htmlFor="name">Subject</label>
+                            <input
+                                type="text"
+                                name="subject"
+                                defaultValue={subject}
+                                onBlur={handleChange}
+                                className="form-control"
+                                id="subject"
+                            />
+                            <div className="validate"></div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="form-group">
+                            <label htmlFor="name">Message</label>
+                            <textarea
+                                className="form-control"
+                                name="message"
+                                defaultValue={message}
+                                onBlur={handleChange}
+                                rows="10"
+                            ></textarea>
+                            </div>
+                        </div>
+                        <div className="row">
+                            {errorMessage && (
+                                <div>
+                                    <p className="error-text">
+                                    <strong>{errorMessage}</strong>
+                                    </p>
+                                </div>
+                            )}
+                        </div>
+                        <div className="row">
+                            <div>
+                                <button type="submit">Send Message</button>
+                            </div>
+                        </div>
                     </form>
                 </div>
+            </div>
             </div>
         </section>
     );
