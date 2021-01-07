8d5666c6d574ff2f0b5d343919a79eec3bf8b49c
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-01-07 11:39:25 -0500

    added custom domain and modified header and resume for new links

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -49,8 +49,8 @@ function Resume () {
 
             </div>
             <div>
-                <object width="100%" height="1000" border="0" data="/react-portfolio/KathleenPehlResume.pdf#view=fitB&toolbar=0" type="application/pdf" aria-label="embedded resume">This browser does not support PDFs. <a href="/KathleenPehlResume.pdf"><span className="icon"><i className="fas fa-download"></i></span>Download my resume here.</a></object>
-                {<br></br>}<a href="/react-portfolio/KathleenPehlResume.pdf"><span className="icon"><i className="fas fa-download"></i></span>Download my resume here.</a>
+                <object width="100%" height="1000" border="0" data="/KathleenPehlResume.pdf#view=fitB&toolbar=0" type="application/pdf" aria-label="embedded resume">This browser does not support PDFs. <a href="/KathleenPehlResume.pdf"><span className="icon"><i className="fas fa-download"></i></span>Download my resume here.</a></object>
+                {<br></br>}<a href="/KathleenPehlResume.pdf"><span className="icon"><i className="fas fa-download"></i></span>Download my resume here.</a>
             </div>
         </div>
     )
