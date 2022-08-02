0db32ac172719907d84d27923e6bf6ec8658576f
Kathleen Pehl <kathleen.pehl@gmail.com>
2022-08-02 09:19:55 -0400

    typo correction

--- src/components/GISH.js
+++ src/components/GISH.js
@@ -11,7 +11,7 @@ function GISH () {
                     <p><a href = "https://www.gish.com/" target="_blank" rel="noreferrer">GISH (the Greatest International Scavenger Hunt)</a> is an annual, week-long scavenger hunt where teams compete (or just have a blast) performing a myriad of tasks that challenge creativity and kindess. Each year, GISH partners with a charity, and a portion of every registration fee goes to support that year's charity. This year, 2022, GISH is supporting <a href="https://www.gish.com/gish-2022-registration-partner-nova-ukraine/" target="_blank" rel="noreferrer">Nova Ukraine</a>.</p>
                 </div>
                 <div>
-                    <p>This site is for one of the tasks for the August 2022 Hunt, taking off from the satirical site <a href="https://birdsarentreal.com/" target="_blank" rel="noreferrer">Birds Aren't Real</a>. This "conspiracy" is entirely toungue-in-check.</p>
+                    <p>This site is for one of the tasks for the August 2022 Hunt, taking off from the satirical site <a href="https://birdsarentreal.com/" target="_blank" rel="noreferrer">Birds Aren't Real</a>. This "conspiracy" is entirely tongue-in-check.</p>
                 </div>
             </div>
         </section>
